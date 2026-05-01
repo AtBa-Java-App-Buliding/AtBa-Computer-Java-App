99957e2f6d5d3d77cf2ce4324d3bf615b36f140f
Atakan Hakkı Bayırlı <atakancapcutvideos11@gmail.com>
2026-05-02 00:42:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.mtvn.atbamovesin;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.printIn("Copyright ©1999 Oracle Corporation");
+    }
+    
+}
